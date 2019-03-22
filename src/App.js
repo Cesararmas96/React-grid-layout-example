3344a82052b7538df826d99f868cacb8a04033c1
Cesar Armas <caal2096@gmail.com>
2019-03-21 23:06:17 -0400

    borrado de las carpetas worker y message-relay

--- src/App.js
+++ src/App.js
@@ -11,23 +11,21 @@ import 'primeicons/primeicons.css';
 // Importando el 'framework' que estamos demo (esto eventualmente se convertirá en un módulo de nodo)
 // Importing he 'framework' that we are demoing (this will eventually become a node module)
 import SubscriberGridLayout from './framework/components/grid';
-import './framework/message-relay/psSubscriber';
 import {subscriberGrid} from './framework/models/subscriberGrid';
 
 // La demostración actual o 'implementación'
 // The actual demo or 'implementation'
 // import ConsumerComponent from './components/consumerComponent';
 
+// Boton de bloqueo de los widtges
 import InvokerComponent from './components/invokerComponent';
+
 import GridElementExample from './grid-elements/gridElementExample';
 
 // Registering the grid-element rendering classes
 // Registro de las clases de representación del elemento de la cuadrícula
 //Cada Elemento que se muestra 
 
-GridElementExample.register("exampleRender", GridElementExample);
-GridElementExample.register("exampleRender", GridElementExample);
-GridElementExample.register("exampleRender", GridElementExample);
 GridElementExample.register("exampleRender", GridElementExample);
 
 
@@ -52,10 +50,13 @@ class MainComponent extends Component{
     
     let layoutMap = { i: '0', x: 0, y: 0, w: 4, h: 3};
     subscriberGrid.addSubscriberGridItem( "exampleRender", layoutMap, subscriptionMap);
+   
     layoutMap = { i: '1', x: 4, y: 0, w: 3, h: 9};
     subscriberGrid.addSubscriberGridItem( "exampleRender", layoutMap, subscriptionMap );
+   
     layoutMap = { i: '2', x: 0, y: 3, w: 2, h: 6};
     subscriberGrid.addSubscriberGridItem( "exampleRender", layoutMap, subscriptionMap );
+   
     layoutMap = { i: '3', x: 7, y: 0, w: 3, h: 7};
     subscriberGrid.addSubscriberGridItem( "exampleRender", layoutMap, subscriptionMap );
 
